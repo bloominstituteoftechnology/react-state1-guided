b1ec0b4a09bee8e11e3d50e8571ed16a3949c019
Ladrillo <gabriel@cabrejas.net>
2022-05-16 18:26:48 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -23,34 +23,16 @@ export default function App() {
   const [state, setState] = useState(initialState)
 
   const onChange = ({ name, value }) => {
-    setState({ ...state, form: { [name]: value } })
+
   }
   const onSubmit = () => {
-    setState({
-      ...state,
-      form: initialForm,
-      todos: state.todos.concat({
-        id: getId(),
-        name: state.form.name,
-        completed: false,
-      }),
-    })
+
   }
   const toggleShouldShow = () => {
-    setState({
-      ...state,
-      displayCompleteds: !state.displayCompleteds
-    })
+
   }
   const toggleStatus = id => () => {
-    setState({
-      ...state,
-      todos: state.todos.map(td => {
-        return td.id == id
-          ? { ...td, completed: !td.completed }
-          : td
-      })
-    })
+
   }
   return (
     <div>
