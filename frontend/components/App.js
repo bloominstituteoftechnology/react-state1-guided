fe1845fe179f6e8d27509098d6e9d2fec691d64b
Ladrillo <gabriel@cabrejas.net>
2022-05-16 18:46:25 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -38,7 +38,7 @@ export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   const onChange = ({ name, value }) => {
-    return {  } // the ACTION
+    return { type:  } // the ACTION object has a type and optional payload
   }
   const onSubmit = () => {
 
