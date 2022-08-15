5d9b72e932b369ac2371d0a908bd35886529d831
Ladrillo <gabriel@cabrejas.net>
2022-08-15 18:21:36 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -27,7 +27,7 @@ const TOGGLE_DISPLAY_COMPLETEDS = 'TOGGLE_DISPLAY_COMPLETEDS'
 const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED'
 
 // STATE RECALCULATOR (reducer)
-function reducer(state, ) { // what it takes as args
+function reducer(state, action) { // what it takes as args
   // what it returns: the next state of the app
 }
 
