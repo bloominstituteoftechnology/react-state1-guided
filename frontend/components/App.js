a22dd16627ebbbbb35a8a9f8eb73625f31f0c898
Ladrillo <gabriel@cabrejas.net>
2022-08-15 18:22:23 +0200

    committing to "lecture"

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -27,7 +27,7 @@ const TOGGLE_DISPLAY_COMPLETEDS = 'TOGGLE_DISPLAY_COMPLETEDS'
 const TOGGLE_COMPLETED = 'TOGGLE_COMPLETED'
 
 // STATE RECALCULATOR (reducer)
-// action is an obj that describes the chance { type: 'CHANGE_INPUT' }
+// an action is an obj that describes the chance { type: 'CHANGE_INPUT', payload: { value } }
 function reducer(state, action) { // what it takes as args
   // what it returns: the next state of the app
 }
